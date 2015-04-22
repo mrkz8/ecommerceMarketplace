a3f0e87e23b1f9824242f82fca27e95773397431
Your Name <ricardojesus.ruizcruz@gmail.com>
2015-04-22 12:17:47 -0500

    Cambios en el composer, integrando build, asi como maquina

---
+++ tests/bootstrap.php
@@ -0,0 +1,46 @@
+<?php
+use Zend\ServiceManager\ServiceManager;
+use Zend\Mvc\Service\ServiceManagerConfig;
+/**
+ * Bootstrap for unit testing
+ *  @author pcarrillo
+ */
+chdir(dirname(__DIR__));
+putenv('APPLICATION_ENV');
+define('REQUEST_MICROTIME', microtime(true));
+error_reporting(E_ALL | E_STRICT);
+include __DIR__ . '/../init_autoloader.php';
+class Bootstrap
+{
+    protected static $config;
+    protected static $sm = null;
+    protected static $smConfig;
+    protected static $listeners;
+    
+    public static function init()
+    {
+        //FIXME dejar esto configurable
+        date_default_timezone_set("America/Mexico_City");
+        self::$config = include'config/application.config.php';
+        self::$smConfig = isset($configuration['service_manager']) ? $configuration['service_manager'] : array();
+        self::$listeners = isset($configuration['listeners']) ? $configuration['listeners'] : array();
+        
+    }
+    public static function getConfig(){
+        return self::$config;
+    }
+    public static function getSm(){
+        if (self::$sm === null){
+            self::$sm = self::configSm();
+        }
+        return self::$sm;
+    }   
+    private static function configSm(){
+        $serviceManager = new ServiceManager(new ServiceManagerConfig(self::$smConfig));
+        $serviceManager->setService('ApplicationConfig', self::$config);
+        $serviceManager->get('ModuleManager')->loadModules();
+        $serviceManager->get('Application')->bootstrap(self::$listeners);
+        return $serviceManager;
+    }
+}
+Bootstrap::init();
